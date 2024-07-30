7e4a2916d25632996cebe7a93e35e24206a2a219
kauz56 <121489265+kauz56@users.noreply.github.com>
2024-07-30 13:37:14 +0200

    Resolve limitations

--- extension.js
+++ extension.js
@@ -1,38 +1,70 @@
 const vscode = require('vscode');
 const path = require('path');
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+// extensions/git/src/util.ts
+const isMacintosh = process.platform === 'darwin';
+const isWindows = process.platform === 'win32';
+
+function normalizePath(path) {
+    // Windows & Mac are currently being handled
+    // as case insensitive file systems in VS Code.
+    if (isWindows || isMacintosh) {
+        return path.toLowerCase();
+    }
+
+    return path;
+}
+
+function pathEquals(a, b) {
+    return normalizePath(a) === normalizePath(b);
+}
+
+// const out = vscode.window.createOutputChannel("fuzzyTab");
+// function print(data) {
+//     out.append(JSON.stringify(data, null, 4));
+// }
+
+let ready = false;
+async function init() {
+    // restored tabs don't automatically restore text documents
+    for (const tabGroup of vscode.window.tabGroups.all) {
+        for (const tab of tabGroup.tabs) {
+            if (tab.input instanceof vscode.TabInputText) {
+                await vscode.workspace.openTextDocument(tab.input.uri).catch(() => {});
+            }
+        }
+    }
+    ready = true;
+}
+
 function activate(context) {
     const lastUsedTimes = new Map();
 
     let disposable = vscode.commands.registerCommand('extension.fuzzyTab', async () => {
-        const openEditors = vscode.workspace.textDocuments;
-        const activeEditor = vscode.window.activeTextEditor;
-        const activeFilePath = activeEditor ? activeEditor.document.uri.fsPath : null;
+        if (!ready) await init();
 
-        const items = openEditors
-            // Filter out non file buffers
-            .filter(editor => editor.uri.fsPath.includes('.'))
-            .filter(editor => !editor.uri.fsPath.endsWith('.git'))
-            // Exclude the currently active document
-            .filter(editor => editor.uri.fsPath !== activeFilePath)
-            .map(editor => ({
-                label: path.basename(editor.fileName),
-                description: path.dirname(editor.uri.fsPath),
-                editor,
-                lastUsed: lastUsedTimes.get(editor.uri.fsPath) || 0
+        const openDocuments = vscode.workspace.textDocuments;
+        const activeEditor = vscode.window.activeTextEditor;
+        const activeFilePath = activeEditor ? activeEditor.document.uri?.fsPath : null;
+
+        const items = openDocuments
+            .filter(d => d.uri.scheme === 'file' || d.uri.scheme === 'vscode-userdata')
+            // extensions/git/src/fileSystemProvider.ts 117
+            .filter(d => !pathEquals(d.uri?.fsPath || '', activeFilePath || ''))
+            .map(d => ({
+                label: path.basename(d.fileName || ''),
+                description: path.dirname(d.uri?.fsPath || ''),
+                document: d,
+                lastUsed: lastUsedTimes.get(d.uri?.fsPath) || 0
             }))
             .sort((a, b) => {
-                // For others, sort based on last used time
                 return b.lastUsed - a.lastUsed;
             });
 
         const selected = await vscode.window.showQuickPick(items, { matchOnDescription: true });
 
         if (selected) {
-            const doc = await vscode.workspace.openTextDocument(selected.editor.uri);
+            const doc = await vscode.workspace.openTextDocument(selected.document.uri);
             vscode.window.showTextDocument(doc);
         }
     });
@@ -41,11 +73,14 @@ function activate(context) {
 
     vscode.window.onDidChangeActiveTextEditor(editor => {
         if (editor && editor.document) {
-            lastUsedTimes.set(editor.document.uri.fsPath, Date.now());
+            const p = editor.document.uri.fsPath;
+            if (p) {
+                lastUsedTimes.set(p, Date.now());
+            }
         }
     });
 }
 exports.activate = activate;
 
-function deactivate() { }
+function deactivate() {}
 exports.deactivate = deactivate;
